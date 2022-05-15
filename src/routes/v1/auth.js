6554836479fd534e2621b22f295470c4b37f7c55
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-15 16:53:49 +0100

    Routes added

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const validation = require('../../middleware/validation');
+const { userSchema } = require('../../models/auth');
 
 const router = express.Router();
 
-router.get('/', (rec, res) => {
-  return res.send({ msg: 'Auth Route' });
+router.post('/register', validation(userSchema), (req, res) => {
+  console.log(req.body);
+  return res.send({ msg: 'Registration successful' });
+});
+
+router.post('/login', validation(userSchema), (req, res) => {
+  console.log(req.body);
+  return res.send({ msg: 'Login successful' });
 });
 
 module.exports = router;
