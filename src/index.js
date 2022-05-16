8ea9b84fc89dc9f05d88fd2cce4bb877d0824a48
Andrejus Ponomariovas <andrejus.pon@gmail.com>
2022-05-16 09:27:53 +0100

    Jwt token, passhash

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-console.log('Hello');
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-
 const authRoutes = require('./routes/v1/auth');
+const contentRoutes = require('./routes/v1/content');
+const { serverPort } = require('./config');
 
 const app = express();
 
@@ -11,14 +10,16 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/', (req, res) => {
-  res.send({ msg: 'Server is running' });
+  return res.send({ msg: 'Server is running' });
 });
 
-app.use('/v1/auth', authRoutes);
+app.use('/v1/auth/', authRoutes);
+app.use('/v1/content/', contentRoutes);
 
 app.all('*', (req, res) => {
   return res.status(404).send({ err: 'Page not found' });
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+app.listen(serverPort, () =>
+  console.log(`Server is running on port ${serverPort}`)
+);
--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,17 +1,68 @@
 const express = require('express');
+const mysql = require('mysql2/promise');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const validation = require('../../middleware/validation');
 const { userSchema } = require('../../models/auth');
+const { mysqlConfig, jwtSecret } = require('../../config');
 
 const router = express.Router();
 
-router.post('/register', validation(userSchema), (req, res) => {
-  console.log(req.body);
-  return res.send({ msg: 'Registration successful' });
+router.post('/register', validation(userSchema), async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mysqlConfig);
+    const passHash = bcrypt.hashSync(req.body.password, 10);
+    const [data] = await con.execute(
+      `
+      INSERT INTO users (email, password)
+      VALUES (${mysql.escape(req.body.email)}, '${passHash}')
+    `
+    );
+    await con.end();
+
+    if (!data.insertId) {
+      return res.status(500).send({ err: 'Server issue - please try again' });
+    }
+
+    return res.send({ msg: 'Registration successful', userId: data.insertId });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ err: 'Server issue - please try again' });
+  }
 });
 
-router.post('/login', validation(userSchema), (req, res) => {
-  console.log(req.body);
-  return res.send({ msg: 'Login successful' });
+router.post('/login', validation(userSchema), async (req, res) => {
+  try {
+    const con = await mysql.createConnection(mysqlConfig);
+    const [data] = await con.execute(
+      `
+      SELECT * FROM users
+      WHERE email = (${mysql.escape(req.body.email)})
+      LIMIT 1
+    `
+    );
+    await con.end();
+
+    if (data.length !== 1) {
+      return res.status(400).send({ err: 'Email or password incorrect' });
+    }
+
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+
+    if (!checkPassword) {
+      return res.status(400).send({ err: 'Email or password incorrect' });
+    }
+
+    const token = jwt.sign({ userId: data[0].id }, jwtSecret);
+
+    return res.send({ msg: 'Login successful', token });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send({ err: 'Server issue - please try again' });
+  }
 });
 
 module.exports = router;
